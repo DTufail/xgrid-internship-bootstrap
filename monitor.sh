e8e046a07b83c2b61c01cb43c6d5ebeade38ba7c
Daniyal <daniyaltufail001@gmail.com>
2026-04-21 11:34:06 +0500

    add health script

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CPU=$(TOP -l 1 | grep "CPU usage" | awk '{print $3}')
+MEM=$(vm_stat | grep "Pages active" | awk '{print $3}' |  tr -d '.')
+DISK=$(df -h / | tail -1 | awk '{print $5}')
+PROC=$(ps aux | wc -l)
+
+echo "{
+   \"cpu\": \"$CPU\",
+   \"memory\": \"$MEM\",
+   \"disk\": \"$DISK\",
+   \"PROCESSES\": \"$PROC\"
+}"
+
