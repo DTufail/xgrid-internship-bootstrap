29af227aa84d724edecf55e1d7b4e0033f3c2b78
DTufail <116206667+DTufail@users.noreply.github.com>
2026-04-21 14:17:11 +0500

    Include timestamp in monitor.sh output
    Added a timestamp to the output of the monitor script.

--- monitor.sh
+++ monitor.sh
@@ -5,11 +5,12 @@ MEM=$(vm_stat | grep "Pages active" | awk '{print $3}' |  tr -d '.')
 DISK=$(df -h / | tail -1 | awk '{print $5}')
 PROC=$(ps aux | wc -l)
 
+TIMESTAMP=$(date)
+
 echo "{
    \"cpu\": \"$CPU\",
    \"memory\": \"$MEM\",
    \"disk\": \"$DISK\",
    \"PROCESSES\": \"$PROC\"
+   \"timestamp\": \"${TIMESTAMP}\"
 }"
-
-"timestamp": "Tue Apr 21 12:57:26 PKT 2026"
