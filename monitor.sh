1a69b1fd87faefd73c9140099895d24290643391
Daniyal <daniyaltufail001@gmail.com>
2026-04-22 10:41:14 +0500

    Fix JSON and add timestamp properly

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CPU=$(TOP -l 1 | grep "CPU usage" | awk '{print $3}')
-MEM=$(vm_stat | grep "Pages active" | awk '{print $3}' |  tr -d '.')
+CPU=$(top -l 1 | grep "CPU usage" | awk '{print $3}')
+MEM=$(vm_stat | grep "Pages active" | awk '{print $3}' | tr -d '.')
 DISK=$(df -h / | tail -1 | awk '{print $5}')
 PROC=$(ps aux | wc -l)
 
